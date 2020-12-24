cef80d0c8f122abe6484a00dd0bf4633540eab64
Camille! <camillebarker@outlook.com>
2020-12-23 18:25:04 -0800

    Initial Commit

---
+++ Tipping Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipping Calculator
+//
+//  Created by Camille Barker on 23/12/2020.
+//  Copyright © 2020 Camille Barker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
